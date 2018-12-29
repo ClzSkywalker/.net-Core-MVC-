8182d884e6eb361f94e3843f24b269a53de78154
1045683477 <1045683477@qq.com>
2018-12-29 22:00:49 +0800

    MVC初级教程

---
+++ DemoCoreStudy教程二/DemoCoreStudy.Models/Sales.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoCoreStudy.Models
+{
+    public class Sales
+    {
+        public int CinemaId { get; set; }
+        public int MovieId { get; set; }
+        public int AudienceCount { get; set; }//卖出多少票
+    }
+}
---
+++ DemoCoreStudy教程二/DemoCoreStudy/Serivce/IMovieService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DemoCoreStudy.Models;
+
+namespace DemoCoreStudy.Serivce
+{
+    public interface IMovieService
+    {
+        Task AddAsync(Movie model);
+        Task<IEnumerable<Movie>> GetByCinemaAsync(int cinemaId);
+    }
+}
