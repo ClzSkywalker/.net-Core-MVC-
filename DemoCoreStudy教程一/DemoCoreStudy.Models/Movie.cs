f8dc4bd44db6a8bf49f324139cbb63a70a0da91c
1045683477 <1045683477@qq.com>
2018-12-29 11:57:09 +0800

    .net core mvc初级

---
+++ DemoCoreStudy教程一/DemoCoreStudy.Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DemoCoreStudy.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CinemaId { get; set; }
+        public string Starring { get; set; }
+        public DateTime ReleseDate { get; set; }
+    }
+}
---
+++ DemoCoreStudy教程一/DemoCoreStudy/Serivce/ICinemaService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DemoCoreStudy.Models;
+
+namespace DemoCoreStudy.Serivce
+{
+    public interface ICinemaService
+    {
+        Task<IEnumerable<Cinema>> GetllAllAsync();//查询所有的Cinema值
+        Task<Cinema> GetByIdAsync(int id);//查询指定ID的Cinema
+        Task AddAsync(Cinema model);//创建Cinema值
+    }
+}
