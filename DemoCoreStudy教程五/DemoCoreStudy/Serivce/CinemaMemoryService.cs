2dced03ae716c541ec94a9f41637bea5787906b6
1045683477 <1045683477@qq.com>
2019-01-02 11:34:42 +0800

    教程五

---
+++ DemoCoreStudy教程五/DemoCoreStudy/Serivce/CinemaMemoryService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoCoreStudy.Models;
+
+namespace DemoCoreStudy.Serivce
+{
+    public class CinemaMemoryService : ICinemaService
+    {
+        private readonly List<Cinema> _cinema=new List<Cinema>();
+
+        public CinemaMemoryService()
+        {
+            _cinema.Add(new Cinema
+            {
+                Id = 1,
+                Name ="天堂电影院",
+                Location = "上海",
+                Capacity = 1000
+            });
+            _cinema.Add(new Cinema
+            {
+                Id=2,
+                Name = "疯人电影院",
+                Location = "北京",
+                Capacity = 10000
+            });
+        }
+
+        public Task<IEnumerable<Cinema>> GetllAllAsync()
+        {
+            return Task.Run(() => _cinema.AsEnumerable());
+        }
+
+        public Task<Cinema> GetByIdAsync(int id)
+        {
+            return Task.Run(() => _cinema.SingleOrDefault(x => x.Id == id));
+        }
+
+        public Task AddAsync(Cinema model)
+        {
+            var maxId = _cinema.Max(x => x.Id);
+            model.Id = maxId + 1;
+            _cinema.Add(model);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ DemoCoreStudy教程五/DemoCoreStudy/Serivce/MovieMemoryService.cs
@@ -0,0 +1,55 @@
+﻿using DemoCoreStudy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoCoreStudy.Serivce
+{
+    public class MovieMemoryService : IMovieService
+    {
+        private readonly List<Movie> _movies=new List<Movie>();
+
+        public MovieMemoryService()
+        {
+            _movies.Add(new Movie
+            {
+                Id = 1,
+                CinemaId = 1,
+                Name = "这个杀手不太冷",
+                ReleseDate = new DateTime(2018, 1, 1),
+                Starring = "Tommy"
+            });
+            _movies.Add(new Movie
+            {
+                Id = 2,
+                CinemaId = 2,
+                Name = "笑傲江湖",
+                ReleseDate = new DateTime(2018, 1, 1),
+                Starring = "Tommy"
+            });
+            _movies.Add(new Movie
+            {
+                Id = 2,
+                CinemaId = 2,
+                Name = "小萝莉与猴神大叔",
+                ReleseDate = new DateTime(2018, 1, 1),
+                Starring = "Tommy"
+            });
+        }
+
+        public Task<IEnumerable<Movie>> GetByCinemaAsync(int cinemaId)
+        {
+            return Task.Run(() => _movies.Where(x=>x.CinemaId==cinemaId));
+        }
+
+        public Task AddAsync(Movie model)
+        {
+            var maxId = _movies.Max(x => x.Id);
+
+            model.Id = maxId + 1;
+            _movies.Add(model);
+            return Task.CompletedTask;
+        }
+    }
+}
